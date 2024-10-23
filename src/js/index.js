5ea017f52449a5191dc86e9fd0b4eb4a51a060dc
match14 <match14@yandex.ru>
2024-10-23 17:29:30 +0300

    blog-detail

--- src/js/index.js
+++ src/js/index.js
@@ -23,26 +23,39 @@ if ($('.js-tabs-page').length) {
 }
 
 // Слайдер кейсов
-const casesSlider = new Swiper('.js-cases-slider',
-{
-	loop:true,
-	navigation: {
-		nextEl: '.js-cases-slider-next',
-		prevEl: '.js-cases-slider-prev',
-	},
-});
+if($('.js-cases-slider').length){
+	const casesSlider = new Swiper('.js-cases-slider',
+	{
+		loop:true,
+		navigation: {
+			nextEl: '.js-cases-slider-next',
+			prevEl: '.js-cases-slider-prev',
+		},
+	});
+}
+
+// Слайдер статей
+if($('.js-articles-slider').length){
+	const articlesSlider = new Swiper('.js-articles-slider',
+	{
+		// loop:true,
+		slidesPerView: 1.16,
+		spaceBetween: 26,
+		navigation: {
+			nextEl: '.js-articles-slider-next',
+			prevEl: '.js-articles-slider-prev',
+		},
+	});
+}
 
 // range-slider
 if($('.js-slider-range').length){
 	$('.js-slider-range').each(function(indx, element){
-		
 		var slider = document.getElementById($(element).attr('id'));
 		var minRange = parseFloat(slider.getAttribute('data-min'));
 		var maxRange = parseFloat(slider.getAttribute('data-max'));
 		var start = parseFloat(slider.getAttribute('data-cur-min'));
 		var finish = parseFloat(slider.getAttribute('data-cur-max'));
-		// var idMinElem = $(element).closest('.js-range').find('.js-slider-range-min').attr('id');
-		// var idMaxElem = $(element).closest('.js-range').find('.js-slider-range-max').attr('id');
 		
 		noUiSlider.create(slider, {
 			start: [start, finish],
@@ -57,58 +70,5 @@ if($('.js-slider-range').length){
 				'max': maxRange
 			},
 		});
-
-		// var snapValues = [
-		// 	document.getElementById(idMinElem),
-		// 	document.getElementById(idMaxElem)
-		// ];
-
-		// var initRange = false;
-
-		// slider.noUiSlider.on('update', function (values, handle) {
-		// 	snapValues[handle].value = values[handle];
-
-		// 	if(initRange == false){
-		// 		if(handle == 1){
-		// 		initRange = true;
-		// 		}
-		// 	}else{
-		// 		$('.js-slider-range-min').trigger("change");
-		// 		$('.js-slider-range-max').trigger("change");
-		// 	}
-		// });
-
-		// snapValues.forEach(function (input, handle) {
-		// 	input.addEventListener('change', function () {
-		// 		var valItem = this.value;
-		// 		var minValItem = parseFloat(snapValues[0].value);
-		// 		var maxValItem = parseFloat(snapValues[1].value);
-
-		// 		if(handle == 0){
-		// 			if((valItem < minRange) || (valItem > maxRange) || (valItem >= maxValItem)){
-		// 				valItem = minRange;
-		// 			}
-		// 		}else{
-		// 			if((valItem < minRange) || (valItem > maxRange) || (valItem <= minValItem)){
-		// 				valItem = maxRange;
-		// 			}
-		// 		}
-		// 		slider.noUiSlider.setHandle(handle, valItem);
-		// 	});
-		// });
-	});
-
-
-	// Проверка полей на ввод цифор
-	$('.js-slider-range-min').on("change keyup input click", function() {
-		if (this.value.match(/[^0-9. ]/g)) {
-			this.value = this.value.replace(/[^0-9. ]/g, '');
-		}
-	});
-
-	$('.js-slider-range-max').on("change keyup input click", function() {
-		if (this.value.match(/[^0-9. ]/g)) {
-			this.value = this.value.replace(/[^0-9. ]/g, '');
-		}
 	});
 }
